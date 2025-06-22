e51501a20f34ad34dc650cae26ec1c7fb28de676
anhtiendzc <tinv65985@gmail.com>
2025-06-22 12:01:46 +0700

    commit message

---
+++ ky-uc-flask/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    images = os.listdir(app.config['UPLOAD_FOLDER'])
+    images = sorted(images, reverse=True)
+    return render_template('index.html', images=images)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(url_for('index'))
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(save_path)
+        return redirect(url_for('index'))
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
